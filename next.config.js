6d55387579ef3b1701f79dd4b02496976fbf3c6e
natureswaysoil <natureswaysoil@gmail.com>
2025-08-11 23:05:50 -0400

    Add Next.js config file

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['your-image-domain.com'], // replace with your actual image domains
-  },
-  experimental: {
-    modern: true,
-  },
-}
+  reactStrictMode: true
+};
 
 module.exports = nextConfig;
